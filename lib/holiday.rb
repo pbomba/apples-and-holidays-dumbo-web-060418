b35fbb381653f07752a75b786121f1cc16f212ea
Paul Bomba <paul@paulbomba.com>
2018-05-31 16:56:30 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -77,6 +77,7 @@ def all_supplies_in_holidays(holiday_hash)
         dayString = day.to_s
         dayString = dayString.gsub(/_/, " ")
         supplyList = supplies.join(", ")
+        supplyList = supplyList.split.map(&:capitalize).join(' ')
         puts "  #{dayString.capitalize}: #{supplyList}"
       end
   end
